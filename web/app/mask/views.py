23628c0474d4ebf997483df93a12a46424743efe
danil <thtrvdnl@gmail.com>
2020-10-27 19:43:29 +0500

    added model and form

--- web/app/mask/views.py
+++ web/app/mask/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from . import models
 
 
 def index(request):
     if request.method == 'POST':
-        print(request.Post.get('image_url', ''))
-    context = {'data': 'hopa'}
+        print(request.POST.get('image_url', ''))
+
+        url = models.Url.objects.create(image_url=request.POST.get('image_url', ''))
+
+        url.save()
+
+    urls = models.Url.objects.all()
+
+    context = {'image_urls': urls}
     return render(request, 'mask/index.html', context)
